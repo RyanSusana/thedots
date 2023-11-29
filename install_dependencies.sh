489329c5a5000f5a341dbbdfd9ffbf9196ea500a
Ryan Susana <ryansusana97@gmail.com>
2023-11-29 21:07:34 +0100

    skip chsh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,7 +9,8 @@ fi
 git clone https://github.com/RyanSusana/thedots $CWD
 cd $CWD
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && rm -f $HOME/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --skip-chsh
+rm -f $HOME/.zshrc
 
 # install homebrew
 yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"

df9cca3bc5b03ee2520d63b7a1bf8546f9234e9e
Ryan Susana <ryansusana97@gmail.com>
2023-12-04 10:53:54 +0100

    ubuntu tested

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,11 +2,16 @@
 if [ "$(uname -s)" = "Linux" ]; then
     source "$(dirname ${BASH_SOURCE[0]})/install_linux.sh"
 fi
-git clone https://github.com/RyanSusana/thedots $HOME/thedots
 git clone https://github.com/RyanSusana/workspace-manager $HOME/workspace-manager
 # oh-my-zsh
 git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 echo '-------------------'
-echo 'Dependencies installed, run the following to set up dotfiles'
-echo 'source "$HOME/thedots/stow.sh"'
-echo 'If on linux, run `sudo chsh -s /bin/zsh && zsh` to go into shell and set as default'
+source "$(dirname ${BASH_SOURCE[0]})/stow.sh"
+
+THESHELL=`getent passwd $(whoami) | awk -F: '{print $7}'`
+if [ "$THESHELL" = "/bin/zsh" ] || [ "$THESHELL" = "/usr/bin/zsh" ]; then
+    source $HOME/.zshrc
+else
+    chsh -s $(which zsh)
+    zsh
+fi
--- install_linux.sh
+++ install_linux.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
+apt update
+#just in case
+apt install sudo
+
+sudo apt install -y gpg curl wget 
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+
 export DEBIAN_FRONTEND=noninteractive
 export TZ=Europe/Amsterdam
-apt update
-apt install -y gpg curl wget sudo
-
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://repo.charm.sh/apt/gpg.key | gpg --dearmor -o /etc/apt/keyrings/charm.gpg
-echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | tee /etc/apt/sources.list.d/charm.list
-
-apt update
-apt install -y git-all zsh bat duf exa fd-find fzf gum jq stow tldr tmux neovim
+sudo apt update
+sudo -E apt install -y vim git-all zsh bat duf exa fd-find fzf gum jq stow tldr tmux ninja-build gettext cmake unzip  
 
 FDFIND=$(which fdfind)
-
 mv $FDFIND "$(dirname $FDFIND)/fd"
 
-chsh $(which zsh)
+BATCAT=$(which batcat)
+mv $BATCAT "$(dirname $BATCAT)/bat"
+# Neovim shit
+git clone --depth 1 -b stable https://github.com/neovim/neovim $HOME/.neovim-source
+pushd $HOME/.neovim-source
+make CMAKE_BUILD_TYPE=RelWithDebInfo 
+make install
+rm -rf $HOME/.neovim-source
+popd

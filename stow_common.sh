ef5a10b012186b67a0dc4f610857434486b9bf8d
Ryan Susana <ryansusana@Ryans-MacBook-Pro.local>
2024-03-28 20:48:10 +0100

    nvim

--- stow_common.sh
+++ stow_common.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 stow --dir "$HOME/thedots" zsh
-stow --dir "$HOME/thedots" neovim
+
+mkdir -p "$HOME/.config/nvim"
+stow --dir "$HOME/thedots" --target="$HOME/.config/nvim" nvim
+
 stow --dir "$HOME/thedots" idea

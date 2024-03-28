1864a370431499799bb7153eccf13eab3354186d
Ryan Susana <ryansusana97@gmail.com>
2024-03-28 11:13:38 +0100

    Update install_macos.sh

--- install_macos.sh
+++ install_macos.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+SRC=$BASH_SOURCE[0]
+
 # install homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-/opt/homebrew/bin/brew bundle install --file="$CWD/Brewfile"
+/opt/homebrew/bin/brew bundle install --file="$SRC/Brewfile"
 
 
 # Neovim

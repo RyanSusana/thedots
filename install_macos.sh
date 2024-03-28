a15b238aab43d8baacaa3a51fbe0ff54e2eaf131
Ryan Susana <ryansusana97@gmail.com>
2024-03-28 11:09:30 +0100

    Update install_macos.sh

--- install_macos.sh
+++ install_macos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # install homebrew
-yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle install --file="$CWD/Brewfile"
 
 

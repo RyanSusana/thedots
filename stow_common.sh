1e37fa2ee2c8cb7dabbfacef4a9b56c93b523248
Ryan Susana <ryansusana@Ryans-MacBook-Pro.local>
2024-03-28 21:45:47 +0100

    Nix

--- stow_common.sh
+++ stow_common.sh
@@ -5,4 +5,7 @@ stow --dir "$HOME/thedots" zsh
 mkdir -p "$HOME/.config/nvim"
 stow --dir "$HOME/thedots" --target="$HOME/.config/nvim" nvim
 
+mkdir -p "$HOME/.config/nix"
+stow --dir "$HOME/thedots" --target="$HOME/.config/nix" nix
+
 stow --dir "$HOME/thedots" idea

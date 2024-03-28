67a7018d430aad4dc9c4d4a223860d9f52676fbb
Ryan Susana <ryansusana97@gmail.com>
2024-03-28 10:37:26 +0100

    Update install_macos.sh

--- install_macos.sh
+++ install_macos.sh
@@ -8,7 +8,8 @@ brew bundle install --file="$CWD/Brewfile"
 if command -v nvim >/dev/null 2>&1; then
   echo "Neovim is already installed. Skipping..."
 else
-  git clone https://github.com/neovim/neovim
-  cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+  git clone https://github.com/neovim/neovim neovim-source
+  pushd neovim-source && make CMAKE_BUILD_TYPE=RelWithDebInfo
   sudo make install
+  popd
 fi
